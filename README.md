e095ad0fbd8d5f5872703f2a16e395536b6fdfcf
Andrew Vann <51332669+chaostheory59@users.noreply.github.com>
2019-06-27 15:06:39 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Train-Activity
+
+This will allow the user to input the train name, the destination, the time of the starting point, and then the frequency. It will calculate the starting point time with current time. It will then post everything to google firebase. It will then post all the results in a row at the top of the screen.
---
+++ assets/javascript/app.js
@@ -0,0 +1,133 @@
+var config = {
+    apiKey: "AIzaSyAoZvjURN2J6Ct93sDZ9XNZLVo0eA8HYIQ",
+    authDomain: "train-table-16fa7.firebaseapp.com",
+    databaseURL: "https://train-table-16fa7.firebaseio.com",
+    projectId: "train-table-16fa7",
+    storageBucket: "",
+    messagingSenderId: "230726302427",
+    appId: "1:230726302427:web:a4660f1efb7a6dcf"
+  };
+  
+  var tName;
+  var tDestination;
+  var tStart;
+  var tFreq;
+
+
+
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+
+  
+  
+  function calcMinutesAway(tStart, tFreq) {
+    console.log(`${tFreq}, ${tStart}`)
+    return parseInt(tFreq) - parseInt(tStart) % parseInt(tFreq);
+  }
+  
+  function calcNextArrival(tminutesAway) {
+    return moment().add(tminutesAway, "m").format("HH:mm");
+  }
+
+  
+
+
+  $("#add-train-btn").on("click", function(event) {
+    event.preventDefault();
+  
+    // Grabs user input
+    var tName = $("#train-name-input").val().trim();
+    var tDestination = $("#destination-input").val().trim();
+    var tStart = moment($("#start-input").val().trim(), "HH:mm").format("HH:mm");
+    var tFreq = $("#frequency-input").val().trim();
+  
+    // Creates local "temporary" object for holding train data
+    
+    // Uploads Train data to the database
+  
+    
+ 
+  
+
+   var tminutesAway = calcMinutesAway(tStart, tFreq);
+		console.log("The next train will be here in: " + tminutesAway +" minutes.");
+
+		//Whenever current time matches next arrival time
+		if (tminutesAway === parseInt(tFreq)) {
+			tminutesAway = 0;
+		}
+
+		//Calculate the next arrival's time
+	  var tnextArrival = calcNextArrival(tminutesAway);
+	    console.log("The next train will be here at: " + tnextArrival);
+    
+      var newTrain = {
+        name: tName,
+        destination: tDestination,
+        start: tStart,
+        Freq: tFreq,
+        nextArriv: tnextArrival,
+        minuteAwa: tminutesAway
+      };
+  
+      console.log(newTrain.name);
+      console.log(newTrain.destination);
+      console.log(newTrain.start);
+      console.log(newTrain.Freq);
+      console.log(newTrain.nextArriv);
+      console.log(newTrain.minuteAwa);
+      database.ref().push(newTrain);
+  // Logs everything to console
+  
+
+  alert("Train Data successfully added");
+
+  // Clears all of the text-boxes
+  $("#train-name-input").val("");
+  $("#destination-input").val("");
+  $("#start-input").val("");
+  $("#rate-input").val("");
+});
+
+database.ref().on("child_added", function(childSnapshot) {
+    console.log(childSnapshot.val());
+  
+    // Store everything into a variable.
+    var trainName = childSnapshot.val().name;
+    var trainDestination = childSnapshot.val().destination;
+    var trainStart = childSnapshot.val().start;
+    var trainFreq = childSnapshot.val().Freq;
+    var Arrival = childSnapshot.val().nextArriv;
+    var minAway = childSnapshot.val().minuteAwa;
+  
+    // train Info
+    console.log(trainName);
+    console.log(trainDestination);
+    console.log(trainStart);
+    console.log(trainFreq);
+  
+    // Prettify the employee start
+    var trainStartP = moment.unix(trainStart).format("HH:mm");
+  
+    // Calculate the months worked using hardcore math
+    // To calculate the months worked
+    
+  
+    // Calculate the total billed rate
+    
+  
+    // Create the new row
+    var newRow = $("<tr>").append(
+      $("<td>").text(trainName),
+      $("<td>").text(trainDestination),
+      $("<td>").text(trainFreq),
+      $("<td>").text(Arrival),
+      $("<td>").text(minAway)
+    );
+  
+    // Append the new row to the table
+    $("#train-table > tbody").append(newRow);
+  });
+  
